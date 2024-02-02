d6a0f33e170e0267dd0f3f2f02587aff41c4c9c8
paulmehmet <122868197+paulmehmet@users.noreply.github.com>
2024-02-02 10:45:59 +0100

    4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -3,7 +3,7 @@
 
 /*
  * File: 4-sum.h
- * Auth: Oni Michael
+ * Auth: Paul Mehmet
  * Desc: Header file that defines a function-like macro SUM(x, y)
  *       that computes the sum of the numbers x and y.
  */
@@ -11,3 +11,4 @@
 #define SUM(x, y) ((x) + (y))
 
 #endif
+  
