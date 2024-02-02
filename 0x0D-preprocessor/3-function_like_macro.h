08d4595a86d734f450731e5c96c7876f1bd5faff
paulmehmet <122868197+paulmehmet@users.noreply.github.com>
2024-02-02 10:45:12 +0100

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,7 +3,7 @@
 
 /*
  * File: 3-function_like_macro.h
- * Auth: Oni S Michael
+ * Auth: Paul Mehmet
  * Desc: Header file that defines a function-like macro ABS(x)
  *       that computes the absolute value of a number x.
  */
