35f115a154f3a6fad0d78379eac27bad49c2d915
paulmehmet <122868197+paulmehmet@users.noreply.github.com>
2024-02-09 11:15:36 +0100

    main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,7 @@
 
 /*
  * File: holberton.h
- * AUth: Gedeon Obae Gekonge
+ * AUth: Paul Mehmet
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x13-bit_manipulation directory.
  */
